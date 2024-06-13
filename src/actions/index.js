57ee1f6923d48f8d0fdad575ad4b1b96f7225124
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-06-13 15:08:27 -0300

    fix: :bug: adicionando action de comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import db from "../../prisma/db";
 
 export async function incrementThumbsUp(post) {
+  // await new Promise((resolve) => setTimeout( resolve, 3500))
+
   await db.post.update({
     where: {
       id: post.id,
@@ -13,4 +16,26 @@ export async function incrementThumbsUp(post) {
       },
     },
   });
+
+  revalidatePath("/");
+  revalidatePath(`/${post.slug}`);
+}
+
+export async function postComment(post, formData) {
+  const author = await db.user.findFirst({
+    where: {
+      username: "anabeatriz_dev",
+    },
+  });
+
+  await db.comment.create({
+    data: {
+      text: formData.get("text"),
+      authorId: author.id,
+      postId: post.id,
+    },
+  });
+
+  revalidatePath("/");
+  revalidatePath(`/${post.slug}`);
 }
