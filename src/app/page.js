953b12e1eaedebfd9535e5c8b216fbfc6dce63a0
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-06-13 15:08:51 -0300

    fix: :bug: refatoracoes e estilizacoes

--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,71 @@
-import { CardPost } from "@/components/CardPost"
-import logger from "@/logger"
+import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
-import styles from './page.module.css'
-import Link from "next/link"
-import db from "../../prisma/db"
+import styles from "./page.module.css";
+import Link from "next/link";
+import db from "../../prisma/db";
 
-async function getAllPosts (page, searchTerm) {
+async function getAllPosts(page, searchTerm) {
   try {
-
-    const where = {}
+    const where = {};
 
     if (searchTerm) {
       where.title = {
         contains: searchTerm,
-        mode: 'insensitive'
-      }
+        mode: "insensitive",
+      };
     }
 
     const perPage = 4;
     const skip = (page - 1) * perPage;
-    const totalItems = await db.post.count({ where })
-    const totalPages = Math.ceil(totalItems / perPage)
-    const prev = page > 1 ? page - 1 : null 
-    const next = page < totalPages ? page + 1 : null
+    const totalItems = await db.post.count({ where });
+    const totalPages = Math.ceil(totalItems / perPage);
+    const prev = page > 1 ? page - 1 : null;
+    const next = page < totalPages ? page + 1 : null;
 
     const posts = await db.post.findMany({
       take: perPage,
       skip,
       where,
-      orderBy: { createdAt: 'desc' },
+      orderBy: { id: "desc" },
       include: {
-        author: true
-      }
-    })
+        author: true,
+        comments: true,
+      },
+    });
 
-    return { data: posts, prev, next }
-    
+    return { data: posts, prev, next };
   } catch (error) {
-    logger.error('Falha ao obter posts', { error })
-    return { data: [], prev: null, next: null }
+    logger.error("Falha ao obter posts", { error });
+    return { data: [], prev: null, next: null };
   }
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = parseInt(searchParams?.page || 1)
-  const searchTerm = searchParams?.q
-  const { data: posts, prev, next } = await getAllPosts(currentPage, searchTerm)
+  const currentPage = parseInt(searchParams?.page || 1);
+  const searchTerm = searchParams?.q;
+  const {
+    data: posts,
+    prev,
+    next,
+  } = await getAllPosts(currentPage, searchTerm);
   return (
     <main className={styles.grid}>
-      {posts.map(post =>  <CardPost key={post.id} post={post} />)}
+      {posts.map((post) => (
+        <CardPost key={post.id} post={post} />
+      ))}
       <div className={styles.links}>
-        {prev && <Link href={{ pathname: '/', query: { page: prev, q: searchTerm } }}>Página anterior</Link>}
-        {next && <Link href={{ pathname: '/', query: { page: next, q: searchTerm } }}>Próxima página</Link>}
+        {prev && (
+          <Link href={{ pathname: "/", query: { page: prev, q: searchTerm } }}>
+            Página anterior
+          </Link>
+        )}
+        {next && (
+          <Link href={{ pathname: "/", query: { page: next, q: searchTerm } }}>
+            Próxima página
+          </Link>
+        )}
       </div>
     </main>
-  )
+  );
 }
